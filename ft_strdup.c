da041e7ef8604151bed58022272ca68e8d64cb48
pbumi <149830750+pbumi@users.noreply.github.com>
2023-12-02 21:05:37 +0200

    strdup edited

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,44 +6,28 @@
 /*   By: pbumidan <pbumidan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:23:16 by pbumidan          #+#    #+#             */
-/*   Updated: 2023/11/18 21:35:20 by pbumidan         ###   ########.fr       */
+/*   Updated: 2023/12/02 21:04:26 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_cpy(char *dest, const char *src)
-{
-	size_t	x;
-
-	x = 0;
-	while (src [x])
-	{
-		dest[x] = src[x];
-		x++;
-	}
-	dest [x] = '\0';
-	return (dest);
-}
-
 char	*ft_strdup(const char *s1)
 {
 	size_t		len;
+	size_t		x;
 	char		*duplicate;
 
+	x = 0;
 	len = ft_strlen(s1);
 	duplicate = (char *)malloc(len + 1);
-	if (duplicate == NULL)
+	if (!duplicate)
+		return (NULL);
+	while (s1[x])
 	{
-		return (0);
+		duplicate[x] = s1[x];
+		x++;
 	}
-	else if (duplicate != NULL)
-	{
-		{
-			ft_cpy(duplicate, s1);
-		}
-		return (duplicate);
-	}
-	else
-		return (0);
+	duplicate[x] = '\0';
+	return (duplicate);
 }
