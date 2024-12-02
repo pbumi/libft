9ac525ce73b75b432eb214dd08feb37c007b4c9b
Paul Bumidang <pbumidan@c2r3p13.hive.fi>
2024-12-02 14:52:35 +0200

    split v1

--- ft_split.c
+++ ft_split.c
@@ -6,105 +6,106 @@
 /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 23:33:55 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/12/02 14:41:46 by pbumidan         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:51:03 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// static char	**freeres(char **res, size_t x)
-// {
-//     if (res == NULL)
-//         return (NULL);
-//     while (x != 0)
-//     {
-//         free(res[x - 1]);
-//         x--;
-//     }
-//     free(res);
-//     return (NULL);
-// }
+static char	**freeres(char **res, size_t x)
+{
+    if (res == NULL)
+        return (NULL);
+    while (x != 0)
+    {
+        free(res[x - 1]);
+        x--;
+    }
+    free(res);
+    return (NULL);
+}
 
-// static size_t	ft_mallocsize(const char *str, char c)
-// {
-// 	size_t	count;
-// 	size_t	i;
+static size_t	ft_mallocsize(const char *str, char c)
+{
+	size_t	count;
+	size_t	i;
 
-// 	i = 0;
-// 	count = 0;
-// 	if (!str)
-// 	{
-// 		return (count);
-// 	}
-// 	while (str[i])
-// 	{
-// 		while (str[i] == c)
-// 		{
-// 			i++;
-// 		}
-// 		if (str[i] != c && str[i] != '\0')
-// 		{
-// 			count++;
-// 		}
-// 		while (str[i] != c && str[i] != '\0')
-// 		{
-// 			i++;
-// 		}
-// 	}
-// 	return (count);
-// }
+	i = 0;
+	count = 0;
+	if (!str)
+	{
+		return (count);
+	}
+	while (str[i])
+	{
+		while (str[i] == c)
+		{
+			i++;
+		}
+		if (str[i] != c && str[i] != '\0')
+		{
+			count++;
+		}
+		while (str[i] != c && str[i] != '\0')
+		{
+			i++;
+		}
+	}
+	return (count);
+}
 
-// static char	**ft_splitter(char **res, const char *s, char c)
-// {
-// 	size_t	i;
-// 	size_t	start;
-// 	size_t	res_i;
+static char	**ft_splitter(char **res, const char *s, char c)
+{
+	size_t	i;
+	size_t	start;
+	size_t	res_i;
 
-// 	i = 0;
-// 	res_i = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		while (s[i] == c)
-// 			i++;
-// 		start = i;
-// 		while (s[i] && s[i] != c)
-// 			i++;
-// 		if (i > start)
-// 		{
-// 			res[res_i] = ft_substr(s, start, i - start);
-// 			{
-// 				if (res[res_i] == NULL)
-// 					return (freeres(res, res_i));
-// 			}
-// 			res_i++;
-// 		}
-// 	}
-// 	res[res_i] = NULL;
-// 	return (res);
-// }
+	i = 0;
+	res_i = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			res[res_i] = ft_substr(s, start, i - start);
+			{
+				if (res[res_i] == NULL)
+					return (freeres(res, res_i));
+			}
+			res_i++;
+		}
+	}
+	res[res_i] = NULL;
+	return (res);
+}
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**res;
-// 	size_t	size;
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	size_t	size;
 
-// 	size = 0;
-// 	if (s == NULL)
-// 	{
-// 		return (NULL);
-// 	}
-// 	size = ft_mallocsize(s, c);
-// 	res = (char **)malloc((sizeof(char *)) * (size + 1));
-// 	if (res == NULL)
-// 	{
-// 		return (NULL);
-// 	}
-// 	if (ft_splitter(res, s, c) == NULL) // If memory allocation fails in splitter, return NULL
-//     {
-//         return (NULL);
-//     }
-// 	return (res);
-// }
+	size = 0;
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	size = ft_mallocsize(s, c);
+	res = (char **)malloc((sizeof(char *)) * (size + 1));
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+	if (ft_splitter(res, s, c) == NULL) // If memory allocation fails in splitter, return NULL
+    {
+        return (NULL);
+    }
+	return (res);
+}
+
 /*NOTES
 ::::Parameters ::::
 _s: The string to be split.
@@ -209,7 +210,7 @@ with a NULL pointer.*/
 // }
 
 /*     VERSION 2   */
-
+/*
 static size_t	ft_get_wrd_len(char const *s, char const c)
 {
 	size_t	i;
@@ -302,7 +303,7 @@ char	**ft_split(char const *s, char const c)
 	if (!allocate_mem(s, c, strings))
 		return (NULL);
 	return (strings);
-}
+}*/
 
 
 
